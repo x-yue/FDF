76e4873a655598d27058678c6cb2150c81eff87f
yuxu <yuxu@github.com>
2018-08-07 21:32:46 +0200

    made change in src to make an int table

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@
 #    By: yuxu <marvin@42.fr>                        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/08/02 14:59:23 by yuxu              #+#    #+#              #
-#    Updated: 2018/08/02 16:56:08 by yuxu             ###   ########.fr        #
+#    Updated: 2018/08/07 14:41:57 by yuxu             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = fdf
-SRCS = main.c srcs/*.c srcs/libft/libft.a
+SRCS = srcs/fdf.c srcs/read.c srcs/draw.c srcs/libft/libft.a
 FLAGS = -L /usr/local/lib -lmlx -framework OpenGL -framework AppKit
 
 all: $(NAME)
--- includes/fdf.h
+++ includes/fdf.h
@@ -6,20 +6,35 @@
 /*   By: yuxu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/01 20:10:17 by yuxu              #+#    #+#             */
-/*   Updated: 2018/08/02 18:38:04 by yuxu             ###   ########.fr       */
+/*   Updated: 2018/08/07 20:54:59 by yuxu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __FDF_H
 # define __FDF_H
 
-#include "mlx.h"
-#include "../srcs/libft/libft.h"
-#include <fcntl.h>
+# include "mlx.h"
+# include "../srcs/libft/libft.h"
+# include <fcntl.h>
 
-void	fdf(int fd);
-int		paint(void);
-int		deal_key(int key, void *param);
-int		readfile(int fd);
+# define BUFF_SIZE 100
+
+typedef	struct		s_table
+{
+	char			*content;
+	int				**table;
+	int				lenth;
+	int				index;
+	int				col;
+	int				lin;
+	char			*word;
+	int				w_len;
+}					t_table;
+
+void				fdf(int fd);
+int					paint(void);
+int					deal_key(int key, void *param);
+int					**treatfile(int fd);
+char				*readfile(int fd);
 
 #endif
